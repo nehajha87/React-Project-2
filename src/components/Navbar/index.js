bc40d95b874c75e31c4042055d9637ed0c556ea1
Neha jha <you@example.com>
2021-02-22 10:57:14 +0530

    first commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Navbar.css';
+import { Nav, NavLink, NavIcon, Bars } from './NavbarElements';
+
+const Navbar = ({ toggle }) => {
+  return (
+    <>
+      <Nav>
+        <NavLink to='/'>p<span className="color">I</span>zza's</NavLink>
+        <NavIcon onClick={toggle}>
+          <p>Menu</p>
+          <Bars />
+        </NavIcon>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Products/data.js
@@ -0,0 +1,126 @@
+import product1 from '../../images/product-1.jpg';
+import product2 from '../../images/product-2.jpg';
+import product3 from '../../images/product-3.jpg';
+import product4 from '../../images/product-4.jpg';
+import product5 from '../../images/product-5.jpg';
+import product6 from '../../images/product-6.jpg';
+import sweet1 from '../../images/sweet3.jpg';
+import sweet2 from '../../images/sweet-2.jpg';
+import sweet3 from '../../images/sweet-3.jpg';
+import sweet4 from '../../images/sweet-4.jpg';
+import sweet5 from '../../images/sweet-5.jpg';
+import sweet6 from '../../images/sweet-6.jpg';
+
+export const productData = [
+  {
+    img: product1,
+    alt: 'Pizza',
+    name: 'Supreme Pizza',
+    desc:
+      'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price:'only at Rupees 299',
+    button: 'Add to Cart'
+  },
+  {
+    img: product2,
+    alt: 'Pizza',
+    name: 'Hawaiian Paradise',
+    desc:
+      ' Hummus And Grilled Zucchini Pizzas',
+    price: 'only at Rupees 399',
+    button: 'Add to Cart'
+  },
+  {
+    img: product3,
+    alt: 'Pizza',
+    name: 'Veggie Overload',
+    desc:
+      'Japanese Pizza with Sushi Rice, Tofu, Edamame, Shiitakes, and Manchego',
+    price: 'only at Rupees 499',
+    button: 'Add to Cart'
+  },
+  {
+    img: product4,
+    alt: 'Pizza',
+    name: 'Sweet Peach ',
+    desc:
+      ' Wrede"s Grilled Pizza with Grapes and Soppressata',
+    price: 'only at Rupees 499',
+    button: 'Add to Cart'
+  },
+  {
+    img: product5,
+    alt: 'Pizza',
+    name: 'Brussels Sprout Pizza',
+    desc:
+      ' Spring Pizza With Peas, Fava Beans, Asparagus, Prosciutto, And Eggs',
+    price: 'only at Rupees 499',
+    button: 'Add to Cart'
+  },
+  {
+    img: product6,
+    alt: 'Pizza',
+    name: 'Alfredo Pizza',
+    desc:
+      ' Blueberry Pizza with Whipped Ricotta + Caramelized Shallots',
+    price: 'only at Rupees 499',
+    button: 'Add to Cart'
+  }
+];
+
+export const productDataTwo = [
+  {
+    img: sweet2,
+    alt: 'Donuts',
+    name: 'Doughlicious',
+    desc:
+      'Belgian chocolate glazed donuts covered in icing with sprinkles on top',
+    price: 'only at Rupees 299',
+    button: 'Add to Cart'
+  },
+  {
+    img: sweet3,
+    alt: 'Ice Cream',
+    name: 'Caramel Wonder',
+    desc:
+      'Vanilla ice cream covered with caramel and chocolate glaze topped with a coco stick',
+    price: 'only at Rupees 399',
+    button: 'Add to Cart'
+  },
+  {
+    img: sweet1,
+    alt: 'Brownie',
+    name: 'Brownie Bunch',
+    desc:
+      'Double fudge brownie squares topped with white chocolate pieces and macadamia nuts',
+    price: 'only at Rupees 499',
+    button: 'Add to Cart'
+  },
+  {
+    img: sweet4,
+    alt: 'Brownie',
+    name: 'Zucchini Cheesecake',
+    desc:
+      'This is the healthy, triple chocolate treat you"ve been waiting for. Rich, decedent, super',
+    price: 'only at Rupees 699',
+    button: 'Add to Cart'
+  },
+  {
+    img: sweet5,
+    alt: 'Brownie',
+    name: 'Thai Desert',
+    desc:
+      'Double fudge brownie squares topped with white chocolate pieces and macadamia nuts',
+    price: 'only at Rupees 499',
+    button: 'Add to Cart'
+  },
+  {
+    img: sweet6,
+    alt: 'Brownie',
+    name: 'Cha Thai Bingsu',
+    desc:
+      'Double fudge brownie squares topped with white chocolate pieces and macadamia nuts',
+    price: 'only at Rupees 599',
+    button: 'Add to Cart'
+  }
+];
